6bc563358cf84310497029fcf18afdc1f01fd48c
Benjamin Florin <benjamin.florin@gmail.com>
2016-07-06 15:19:31 -0400

    Track trial databases directly

--- src/DatabaseStore.php
+++ src/DatabaseStore.php
@@ -62,6 +62,10 @@ class DatabaseStore
         $output_db = new \stdClass();
         $output_db->short_name = $input_db->name;
         $output_db->url = $input_db->meta->enable_proxy ? $this->proxify($input_db->url) : $input_db->url;
+
+        if (isset($input_db->enable_trial)) {
+            $output_db->enable_trial = true;
+        }
         return $output_db;
     }
 
